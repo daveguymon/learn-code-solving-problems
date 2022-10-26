e4755c98b2ed8657c653615b67cf1c3c49d42093
David Guymon <david.guymon@mx.com>
2022-10-26 15:15:15 -0600

    working solution of who_is_in_the_middle

---
+++ chapter_02/exercises/who_is_in_the_middle.py
@@ -0,0 +1,11 @@
+bowl1 = int(input())
+bowl2 = int(input())
+bowl3 = int(input())
+
+bowls = [bowl1, bowl2, bowl3]
+
+sorted_bowls = sorted(bowls)
+
+mamas_bowl = sorted_bowls[1]
+
+print(mamas_bowl)
