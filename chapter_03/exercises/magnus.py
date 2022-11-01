9352f27aa911efce162f9b5741465643ecd8c3c4
David Guymon <david.guymon@mx.com>
2022-11-01 13:51:43 -0600

    refactored working solution for magnus.py exercise

--- chapter_03/exercises/magnus.py
+++ chapter_03/exercises/magnus.py
@@ -2,14 +2,15 @@ some_word = input()
 honi_blocks = []
 
 for character in some_word:
-  if (not honi_blocks or honi_blocks[-1] == 'I') and character == 'H':
-    honi_blocks.append(character)
-  elif honi_blocks and honi_blocks[-1] == 'H' and character == 'O':
-    honi_blocks.append(character)
-  elif honi_blocks and honi_blocks[-1] == 'O' and character == 'N':
-    honi_blocks.append(character)
-  elif honi_blocks and honi_blocks[-1] == 'N' and character == 'I':
-    honi_blocks.append(character)
+  if not honi_blocks or honi_blocks[-1] == 'I':
+    if character == 'H':
+      honi_blocks.append(character)
+  elif honi_blocks:
+    if (honi_blocks[-1] == 'H' and character == 'O' or
+          honi_blocks[-1] == 'O' and character == 'N' or
+          honi_blocks[-1] == 'N' and character == 'I'):
+            honi_blocks.append(character)
 
 honi_string = ''.join(honi_blocks)
+
 print(honi_string.count('HONI'))
